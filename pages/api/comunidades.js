02ef4583c8dde1d4abfc3eee8dd0baa0e152358c
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-18 18:19:16 -0300

    feat: create a new record

--- pages/api/comunidades.js
+++ pages/api/comunidades.js
@@ -1,6 +1,25 @@
+import { SiteClient } from 'datocms-client'
+
 export default async function recebeRequests(req, res) {
-    const token = 'ea7b940f6719365d45c600974aa4ac'
-    res.json({
-        dados: "Dado"
+    if (req.method === 'POST') {
+        const token = 'ea7b940f6719365d45c600974aa4ac'
+        const client = new SiteClient(token)
+
+        const record = await client.items.create({
+            itemType: "973327",
+            title: "Teste",
+            imageUrl: "https://github.com/omariosouto.png",
+            creatorSlug: 'omario'
+        })
+
+        res.json({
+            dados: "Dado",
+            record: record
+        })
+        return
+    }
+
+    res.status(404).json({
+        message: "Erro"
     })
 }
